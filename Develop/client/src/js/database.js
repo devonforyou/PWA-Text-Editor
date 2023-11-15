a7d4b78418afa557e5db32659f630d22f95539ba
Devon Hua <huadevon@gmail.com>
2023-11-15 17:03:11 -0600

    set up instructions for install

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// add content to DB
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = await store.put({ id: "1", content: content });
+  await tx.done;
+  console.log(request)
+  return request;
+  }
+  
+  // get content from DB
+  export const getDb = async () => {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = await store.get("1");
+    await tx.done;
+    console.log(request ? 'IndexedDB data found' : 'No IndexedDB data found')
+    console.log(request)
+    return request;
+  };
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,23 @@
 const butInstall = document.getElementById('buttonInstall');
+var savedPrompt;
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
-
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+ //   The event triggers on page load, but we want to hold on to the event for later.
+    savedPrompt = event;
+ 
+    // the event only fires if the page recognizes the manifest, so if it does we can show the button
+ });
+ 
+ butInstall.addEventListener('click', async () => {
+    // Show the install prompt that we saved
+    if (savedPrompt) {
+      savedPrompt.prompt();
+    }
+ });
+ 
+ window.addEventListener('appinstalled', (event) => {
+    // do an alert saying "App installed"
+    window.alert('App installed');
+ });
